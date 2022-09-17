b590d3c0710a121ed535ca82bc4a180f93969c00
Jake Ireland <jakewilliami@icloud.com>
2022-09-17 17:59:40 +1200

    Add Splunk keywords with naïve regex
    Added some keyword lists:
      - splunk-builtin-functions
      - splunk-eval-functions
      - splunk-transforming-functions
      - splunk-language-constants
    
    Regex still to implement:
      - splunk-digits
      - splunk-macro-names
      - splunk-operators
      - splunk-variables
      - splunk-comparison-or-assignment
    
    Currently, only the splunk-builtin-functions and string variables are
    actually doing anything in terms of highlighting, and will only work
    if delimited by space.

--- splunk-mode.el
+++ splunk-mode.el
@@ -1,9 +1,11 @@
-;;; splunk-mode.el --- Major Mode for editing Splunk SPL code -*- lexical-binding: t -*-
+;;; splunk-mode.el --- Major Mode for editing Splunk SPL source code -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2022 Jake Ireland <jakewilliami@icloud.com>
 
+;; Version: 1.0
 ;; Author: Jake Ireland <jakewilliami@icloud.com>
 ;; URL: https://github.com/jakewilliami/splunk-mode
+;; Keywords: languages
 
 ;; Permission is hereby granted, free of charge, to any person
 ;; obtaining a copy of this software and associated documentation
@@ -27,7 +29,7 @@
 
 ;;; Commentary:
 ;;
-;; Major Mode for editing Splunk Search Processing Language (SPL) code
+;; Major Mode for editing Splunk Search Processing Language (SPL) source code.
 ;;
 ;; Following pages are from 
 ;; https://docs.splunk.com/Documentation/Splunk/9.0.1/
@@ -35,12 +37,102 @@
 ;; SPL: Search/Aboutthesearchlanguage
 ;; SPL Syntax: SearchReference/UnderstandingSPLsyntax
 
-;; TODO: Comments
-;; TODO: Syntax table
-;; TODO: Extensions
+;; TODO: Add highlighting 
 ;; TODO: jump to the opposite side of the blocks with C-M-f and C-M-b
 ;; I think tuareg-mode can do that with similar end-like block, we try
 ;; to steal their approach
+
+
+(defvar splunk-mode-hook nil)
+
+;; (defgroup splunk ()
+  ;; "Major mode for Splunk source code."
+  ;; :group 'languages
+  ;; :prefix "splunk-")
+
+(defconst splunk-mode-syntax-table
+  (with-syntax-table (copy-syntax-table)
+    ;; C/C++ style comments
+	(modify-syntax-entry ?/ ". 124b")
+	(modify-syntax-entry ?* ". 23")
+	(modify-syntax-entry ?\n "> b")
+    ;; Chars are the same as strings
+    (modify-syntax-entry ?' "\"")
+    (syntax-table))
+  "Syntax table for `splunk-mode'.")
+
+(eval-and-compile
+  (defconst splunk-builtin-functions
+    '("abstract" "accum" "addcoltotals" "addinfo" "addtotals"
+      "analyzefields" "anomalies" "anomalousvalue" "append"
+      "appendcols" "appendpipe" "arules" "associate" "audit"
+      "autoregress" "bucket" "bucketdir" "chart" "cluster"
+      "collect" "concurrency" "contingency" "convert" "correlate"
+      "crawl" "datamodel" "dbinspect" "dbxquery" "dbxlookup"
+      "dedup" "delete" "delta" "diff" "dispatch" "erex" "eval"
+      "eventcount" "eventstats" "extract" "fieldformat" "fields"
+      "fieldsummary" "file" "filldown" "fillnull" "findtypes"
+      "folderize" "foreach" "format" "from" "gauge" "gentimes"
+      "geostats" "head" "highlight" "history" "input" "inputcsv"
+      "inputlookup" "iplocation" "join" "kmeans" "kvform"
+      "loadjob" "localize" "localop" "lookup" "makecontinuous"
+      "makemv" "makeresults" "map" "metadata" "metasearch"
+      "multikv" "multisearch" "mvcombine" "mvexpand" "nomv"
+      "outlier" "outputcsv" "outputlookup" "outputtext" "overlap"
+      "pivot" "predict" "rangemap" "rare" "regex" "relevancy"
+      "reltime" "rename" "replace" "rest" "return" "reverse"
+      "rex" "rtorder" "run" "savedsearch" "script" "scrub"
+      "search" "searchtxn" "selfjoin" "sendemail" "set" "setfields"
+      "sichart" "sirare" "sistats" "sitimechart" "sitop" "sort"
+      "spath" "stats" "strcat" "streamstats" "table" "tags"
+      "tail" "timechart" "top" "transaction" "transpose" "trendline"
+      "tscollect" "tstats" "typeahead" "typelearner" "typer" "uniq"
+      "untable" "where" "x11" "xmlkv" "xmlunescape" "xpath"
+      "xyseries"))
+  (defconst splunk-eval-functions
+     '("abs" "acos" "acosh" "asin" "asinh" "atan" "atan2" "atanh"
+       "case" "cidrmatch" "ceiling" "coalesce" "commands" "cos"
+       "cosh" "exact" "exp" "floor" "hypot" "if" "in" "isbool"
+       "isint" "isnotnull" "isnull" "isnum" "isstr" "len" "like"
+       "ln" "log" "lower" "ltrim" "match" "max" "md5" "min" "mvappend"
+       "mvcount" "mvdedup" "mvfilter" "mvfind" "mvindex" "mvjoin"
+       "mvrange" "mvsort" "mvzip" "now" "null" "nullif" "pi" "pow"
+       "printf" "random" "relative_time" "replace" "round" "rtrim"
+       "searchmatch" "sha1" "sha256" "sha512" "sigfig" "sin" "sinh"
+       "spath" "split" "sqrt" "strftime" "strptime" "substr" "tan"
+       "tanh" "time" "tonumber" "tostring" "trim" "typeof" "upper"
+       "urldecode" "validate"))
+  (defconst splunk-transforming-functions
+     '("avg" "count" "distinct_count" "estdc" "estdc_error" "eval"
+       "max" "mean" "median" "min" "mode" "percentile" "range"
+       "stdev" "stdevp" "sum" "sumsq" "var" "varp" "first" "last"
+       "list" "values" "earliest" "earliest_time" "latest"
+       "latest_time" "per_day" "per_hour" "per_minute" "per_second"
+       "rate"))
+  ;; (defconst splunk-macro-names '())
+  ;; (defconst splunk-operators '())
+  (defconst splunk-language-constants
+     '("as" "by" "or" "and" "over" "where" "output" "outputnew" "NOT"
+       "true" "false"))
+  ;; (defconst splunk-variables '())
+  ;; (defconst splunk-comparison-or-assignment '())
+)
+
+(defconst splunk-highlights
+  `((,(regexp-opt splunk-builtin-functions 'symbols) . font-lock-keyword-face)))
+
+;;;###autoload
+(define-derived-mode splunk-mode prog-mode "splunk"
+  "Major Mode for editing Splunk source code."
+  :syntax-table splunk-mode-syntax-table
+  (setq font-lock-defaults '(splunk-highlights))
+  (setq-local comment-start "// "))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.spl\\'" . splunk-mode))
+(add-to-list 'auto-mode-alist '("\\.splunk\\'" . splunk-mode))
+
+(provide 'splunk-mode)
 
 ;;; splunk-mode.el ends here
 
