dbf1367cc554295b7db063d9e222acb1702ea408
Jake Ireland <jakewilliami@icloud.com>
2022-09-17 17:20:46 +1200

    Initial commit of splunk-mode.el
    Added author, license and commentary details to main splunk-mode.el
    file, as well as added a broad to-do list to guide the development of
    this codebase.

---
+++ splunk-mode.el
@@ -0,0 +1,46 @@
+;;; splunk-mode.el --- Major Mode for editing Splunk SPL code -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Jake Ireland <jakewilliami@icloud.com>
+
+;; Author: Jake Ireland <jakewilliami@icloud.com>
+;; URL: https://github.com/jakewilliami/splunk-mode
+
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Commentary:
+;;
+;; Major Mode for editing Splunk Search Processing Language (SPL) code
+;;
+;; Following pages are from 
+;; https://docs.splunk.com/Documentation/Splunk/9.0.1/
+;; 
+;; SPL: Search/Aboutthesearchlanguage
+;; SPL Syntax: SearchReference/UnderstandingSPLsyntax
+
+;; TODO: Comments
+;; TODO: Syntax table
+;; TODO: Extensions
+;; TODO: jump to the opposite side of the blocks with C-M-f and C-M-b
+;; I think tuareg-mode can do that with similar end-like block, we try
+;; to steal their approach
+
+;;; splunk-mode.el ends here
+
